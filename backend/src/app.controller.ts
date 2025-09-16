432a0ca60d698b27ef0e4f2a2f02190e5f285426
RafaelGusto <rafael.gustojunior@gmail.com>
2025-09-16 00:01:29 -0300

    token

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -12,8 +12,11 @@ export class AppController {
   }
 
   @Post('generate-token')
-  async generateToken(): Promise<string> {
+  async generateToken(): Promise<any> {
     const app_id = '1272065253262279';
+    const client_secret = '2TZsC2wW54wANLQexgLT1UNxtNmqjO5n';
+    const code = 'TG-68c8af025f7a5a0001afd0c5-2467395895';
+    const redirect_uri = 'https://secure-ox-cuddly.ngrok-free.app/';
 
     const url_principal = 'https://api.mercadolibre.com/oauth/token';
 
@@ -33,6 +36,32 @@ export class AppController {
       },
     });
     console.log(teste.data, 'TESTE AQUIII');
-    return 'oi';
+    return teste.data;
+  }
+
+  @Post('generate-access-token')
+  async generateAccessToken(): Promise<string> {
+    const app_id = '1272065253262279';
+    const client_secret = '2TZsC2wW54wANLQexgLT1UNxtNmqjO5n';
+    const refresh_token = 'TG-68c8af6bdb6ccb000162675d-2467395895';
+
+    const url_principal = 'https://api.mercadolibre.com/oauth/token';
+
+    const teste = await axios({
+      method: 'post',
+      url: url_principal,
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+      data: {
+        grant_type: 'refresh_token',
+        client_id: app_id,
+        client_secret: client_secret,
+        refresh_token,
+      },
+    });
+
+    return teste.data;
   }
 }
